bc4cffc75561a03a9023beac7d3035ab615dfe7b
Ruben Cali <48370133+RubenCali@users.noreply.github.com>
2020-01-16 23:19:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+let menu = document.getElementById("menukaart");
+let leeg = [];
+let menukaart = [
+  "Spaghetti met ham en kaas",
+  "Macaroni met ham en kaas",
+  "Tagliatelli met ham en kaas",
+  "Spaghetti met spinazie en room",
+  "Macaroni met spinazie en room",
+  "Tagliatelli met spinazie en room",
+  "Spaghetti met gehakt-tomatensaus en kaas",
+  "Macaroni met gehakt-tomatensaus en kaas",
+  "Tagliatelli met gehakt-tomatensaus en kaas",
+  " Spaghetti met spekjes, spinazie en room",
+  " Macaroni met spekjes, spinazie en room",
+  " Tagliatelli met spekjes, spinazie en room",
+  " Spaghetti met courgette en tomatensaus",
+  "Macaroni met courgette en tomatensaus",
+  " Tagliatelli met courgette en tomatensaus",
+  " Lasagne met courgette en tomatensaus",
+  " Lasagne met room, doperwten en tomatensaus",
+  " Lasagne met spinazie en tomatensaus en kaas"
+];
+
+
+document.getElementById("menukaart") = menukaart;
+
+let menukaart1 = menukaart[0];
+let menukaart2 = menukaart[1];
+let menukaart3 = menukaart[2];
+let menukaart4 = menukaart[3];
+let menukaart5 = menukaart[4];
+let menukaart6 = menukaart[5];
+let menukaart7 = menukaart[6];
+let menukaart8 = menukaart[7];
+let menukaart9 = menukaart[8];
+let menukaart10 = menukaart[9];
+let menukaart11 = menukaart[10];
+let menukaart12 = menukaart[11];
+let menukaart13 = menukaart[12];
+let menukaart14 = menukaart[13];
+let menukaart15 = menukaart[14];
+let menukaart16 = menukaart[15];
+let menukaart17 = menukaart[16];
+let menukaart18 = menukaart[17];
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+addNumberToArrayElement(number= 20);
+function addNumberToArrayElement(number){
+    for (let i = 0; 1 < number.length; i++){
+        console.log(numbers[i] + number)
+    }
+  }
+
+
